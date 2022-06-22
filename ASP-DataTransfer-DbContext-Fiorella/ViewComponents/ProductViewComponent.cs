b04822356ba648a0f438e7bf82a4d0176502c428
Umudov Umud <umudau@code.edu.az>
2022-06-23 00:58:32 +0400

    feat: viwecomponents

---
+++ ASP-DataTransfer-DbContext-Fiorella/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using ASP_DataTransfer_DbContext_Fiorella.DataContext;
+using ASP_DataTransfer_DbContext_Fiorella.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_DataTransfer_DbContext_Fiorella.ViewComponents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync(int take)
+        {
+            List<Product> products = _context.Products.Include(p => p.Category).Take(take).ToList();
+
+            return View(await Task.FromResult(products));
+
+        }
+    }
+}
