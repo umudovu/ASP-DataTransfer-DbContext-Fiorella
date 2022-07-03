824896cd64667d36b0b5eb555db49d44e3f31843
Umudov Umud <umudau@code.edu.az>
2022-07-03 13:58:32 +0400

    feat: category Create, Remove

--- ASP-DataTransfer-DbContext-Fiorella/Areas/Admin/Controllers/CategoryController.cs
+++ ASP-DataTransfer-DbContext-Fiorella/Areas/Admin/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 
 namespace ASP_DataTransfer_DbContext_Fiorella.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly AppDbContext _context;
@@ -26,9 +27,27 @@ namespace ASP_DataTransfer_DbContext_Fiorella.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            
 
-            return Content($"{category.Name} {category.Description}");
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("index","Category");
+        }
+
+        public IActionResult Remove(int? id)
+        {
+
+            if (id == null) return NotFound();
+
+            Category category = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null) return NotFound();
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("index", "Category");
         }
 
     }
